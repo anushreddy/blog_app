02bfff7dcdb49029e0e555f081c4382850b6ba4a
Anush Reddy <anushreddy04@gmail.com>
2019-09-11 12:23:40 -0500

    forced signin to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,21 @@
 class CommentsController < ApplicationController
   before_action :set_article
-  
-  def create
-    @comment = @article.comments.build(comment_params)
-    @comment.user = current_user
 
-    if@comment.save
-      flash[:notice] = "Comment has been created"
+  def create
+    unless current_user
+      flash[:alert] = "You need to signin or signup first."
+      redirect_to new_user_session_path
     else
-      flash[:notice] = "Comment has not been created"
+      @comment = @article.comments.build(comment_params)
+      @comment.user = current_user
+
+      if@comment.save
+        flash[:notice] = "Comment has been created"
+      else
+        flash[:notice] = "Comment has not been created"
+      end
+      redirect_to article_path(@article)
     end
-    redirect_to article_path(@article)
   end
 
   private
